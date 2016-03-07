6978adc55eff11f06a0673d204720ca9614ddec2
Sebouh Bardakjian <sebouh_bardakjian@hotmail.com>
2016-03-07 00:48:09 -0500

    Added charmander model with idle and walking animations

---
+++ Evolution_Tactics/Assets/Scripts/Walking.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Walking : MonoBehaviour {
+
+	private Animator anim;
+	private float vert;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		vert = Input.GetAxis("Vertical");
+		anim.SetFloat("Walk", vert);
+
+		if (vert > 0.1f){
+			transform.position += transform.forward * vert * speed * Time.deltaTime;
+		}
+	}
+}
