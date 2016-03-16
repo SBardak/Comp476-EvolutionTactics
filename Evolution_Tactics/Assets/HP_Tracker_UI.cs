579cb7bf68ce365f7c0c3cabcdf56cf21815e33c
Kerupt <kerput@hotmail.com>
2016-03-16 19:36:07 -0400

    HP tracker Still to do: Look of tracker

---
+++ Evolution_Tactics/Assets/HP_Tracker_UI.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HP_Tracker_UI : MonoBehaviour {
+    [SerializeField]
+    Text _text;
+    [SerializeField]
+    Image _inner;
+
+    float initialW;
+    RectTransform _rect;
+
+    void Start()
+    {
+        _rect = _inner.GetComponent<RectTransform>();
+
+        initialW = _rect.sizeDelta.x;
+    }
+
+    public void SetColor(Color c)
+    {
+        _inner.color = c;
+    }
+
+    public void SetHP(int current, int max)
+    {
+        if (max == 0)
+            return;
+
+        _text.text = current + " / " + max;
+
+        float per = (max - current) / (float)max;
+        var d = _rect.sizeDelta;
+        d.x = initialW - (per * initialW);
+        _rect.sizeDelta = d;
+
+        /* Movement variant */
+        //var w = rect.sizeDelta.x;
+
+        //var p = rect.localPosition;
+
+        //float per = (max - current) / (float)max;
+        //p.x = (per * -w);
+
+        //rect.localPosition = p;
+    }
+}
---
+++ Evolution_Tactics/Assets/Scripts/UI/HP_Tracker.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HP_Tracker : MonoBehaviour {
+    [SerializeField]
+    GameObject target;
+    public float xOffset;
+    public float yOffset;
+    public Image HP_PREFAB;
+
+    private Image _instantiated;
+    private RectTransform _rect;
+
+    public int DEBUG_HP_VALUE = 20, DEBUG_HP_VALUE_MAX = 20;
+
+    [SerializeField]
+    Color c; // TODO: Change so that the color can be generated at launch
+
+    void Start()
+    {
+        if (HP_PREFAB == null)
+        {
+            Debug.LogError("HP_Tracker requires HP prefab");
+            Destroy(this);
+            return;
+        }
+        target = gameObject;
+        _instantiated = Instantiate(HP_PREFAB);
+        _instantiated.transform.SetParent(GameObject.Find("Canvas").transform, false);
+        _rect = _instantiated.GetComponent<RectTransform>();
+
+        _instantiated.GetComponent<HP_Tracker_UI>().SetColor(c);
+    }
+	
+	void Update () {
+        var screenPos = Camera.main.WorldToScreenPoint(target.transform.position);
+
+        screenPos.x -=  xOffset;
+        screenPos.y -= yOffset;
+
+        _rect.position = screenPos;
+
+        // TODO: REMOVE ME
+        SetHP();
+	}
+
+    void SetHP()
+    {
+        _instantiated.GetComponent<HP_Tracker_UI>().SetHP(DEBUG_HP_VALUE, DEBUG_HP_VALUE_MAX);
+    }
+}
