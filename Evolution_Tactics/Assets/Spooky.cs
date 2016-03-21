9570f478415a957aa66bae0b40b3b754a69a6552
Kerupt <kerput@hotmail.com>
2016-03-21 05:15:51 -0400

    Only the best for our game!

---
+++ Evolution_Tactics/Assets/Spooky.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Spooky : MonoBehaviour {
+
+    MovieTexture test;
+    RawImage img;
+    AudioSource audio;
+
+    int i = 0;
+
+    void Start()
+    {
+        img = GetComponent<RawImage>();
+        test = (MovieTexture)img.texture;
+
+        test.loop = true;
+        //GetComponent<Renderer>().material.mainTexture = test;
+        //img.material.mainTexture = test;
+
+        audio = GetComponent<AudioSource>();
+        audio.clip = test.audioClip;
+
+        SetPos();
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp(KeyCode.P))
+        {
+            if (!test.isPlaying)
+            {
+                foreach (var t in GameObject.FindObjectOfType<Canvas>().GetComponentsInChildren<Image>())
+                {
+                    t.sprite = null;
+                }
+                foreach (var t in GameObject.FindObjectsOfType<Tile>())
+                {
+                    t.GetComponent<Renderer>().material.mainTexture = test;
+                }
+
+                img.enabled = true;
+                test.Play();
+                audio.Play();
+            }
+            else
+            {
+                i = 0;
+
+                img.enabled = false;
+                test.Stop();
+                audio.Stop();
+            }
+        }
+        if (test.isPlaying)
+        {
+            SetPos();
+            ++i;
+        }
+    }
+
+    void SetPos()
+    {
+        var pos = img.rectTransform.position;
+
+        if (i < 60)
+        {
+            pos.x = 100;
+            pos.y = Screen.height - 100;
+        }
+        else if (i < 120)
+        {
+            pos.x = Screen.width - 100;
+            pos.y = Screen.height - 100;
+        }
+        else if (i < 180)
+        {
+            pos.x = Screen.width - 100;
+            pos.y = 100;
+        }
+        else if (i < 240)
+        {
+            pos.x = 100;
+            pos.y = 100;
+        }
+        else {
+            i = 0;
+        }
+
+        img.rectTransform.position = pos;
+    }
+
+
+}
